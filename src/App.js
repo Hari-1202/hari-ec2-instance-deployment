870f6caf195ba5a5ec381a442e32ef696a988514
Hari-1202"git config --global user.name Hari-1202git config --global user.name Hari-1202 <hariharandhanraj0203@gmail.com>
2024-08-26 15:23:44 +0530

    Checking defloyment after ssh termination

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Modified to use ec2 instance.
+          Modified to use ec2 instance deploy 2.
         </p>
         <a
           className="App-link"
